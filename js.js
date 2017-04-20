4d380a72398e715b1b7b868f194890292f6609b9
yuks01 <hiroyuki.aonuma@gmail.com>
2017-04-20 17:29:45 +0900

    Jose Rizal Tribute Page

---
+++ js.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(){
+					Typed.new('#typed', {
+						stringsElement: document.getElementById('typed-strings'),
+						typeSpeed: -5000
+					});
+					window.setInterval(function() {
+					  var elem = document.getElementById('typedContainer');
+					  elem.scrollTop = elem.scrollHeight;
+					  var hiddenOverflow = document.getElementById('hiddenOverflow');
+					  // $('hiddenOverflow').css("overflow", "hidden");
+					  // $('typedContainer').css({"overflow":"auto", "padding-right":"20px"});
+					  var parent = document.getElementById('typedContainer');
+						var child = document.getElementById('hiddenOverflow');
+						child.style.paddingRight = child.offsetWidth - child.clientWidth + "px";
+						console.log(child.style.paddingRight);
+						console.log(child.clientWidth);
+					  // console.log(elem.scrollHeight);
+					  // console.log(elem.scrollTop);
+					}, 200);
+				});
